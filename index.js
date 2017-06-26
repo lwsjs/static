3a49436264854888a0bf5dfd004b971bcb044662
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-06-26 22:44:01 +0100

    desc

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ module.exports = MiddlewareBase => class Stack extends MiddlewareBase {
         description: 'Root directory, defaults to the current directory.'
       },
       {
-        name: 'static.maxage', type: Number, description: 'Browser cache max-age in milliseconds.'
+        name: 'static.maxage', type: Number, description: 'Cache max-age (in milliseconds) applied to all static resources served.'
       },
       {
-        name: 'static.defer', type: Boolean, description: 'If true, serves after `yield next`, allowing any downstream middleware to respond first.'
+        name: 'static.defer', type: Boolean, description: 'If true, serves after `await next`, allowing any downstream middleware to respond first.'
       },
       {
         name: 'static.index',
